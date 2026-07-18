634d177c2be993a060989910c94096aa05217dc7
arunt2005 <arunt2005@gmail.com>
2026-07-18 20:35:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,120 @@
+
+
+let originalQuestions = [
+  {
+    "q": "A grassland is a type of habitat characterised by extremely hot and dry weather.",
+    "options": ["True", "False"],
+    "answer": "False"
+  },
+  {
+    "q": "Camels in cold deserts have two humps.",
+    "options": ["True", "False"],
+    "answer": "True"
+  },
+  {
+    "q": "The leaves of grassland plants are narrow, which helps in reducing water loss.",
+    "options": ["True", "False"],
+    "answer": "True"
+  },
+  {
+    "q": "Dolphins have blubber that helps them in breathing underwater.",
+    "options": ["True", "False"],
+    "answer": "False"
+  },
+  {
+    "q": "Sunflower seeds, almonds, and vegetable oil are good sources of fats.",
+    "options": ["True", "False"],
+    "answer": "True"
+  },
+  {
+    "q": "Water is not an important element of a balanced diet.",
+    "options": ["True", "False"],
+    "answer": "False"
+  },
+  {
+    "q": "Peels of fruits such as apples contain nutrients.",
+    "options": ["True", "False"],
+    "answer": "True"
+  },
+  {
+    "q": "Excessive intake of fats increases the amount of cholesterol in the blood.",
+    "options": ["True", "False"],
+    "answer": "True"
+  },
+  {
+    "q": "Vitamin D helps in the clotting of blood in case of cuts or injuries.",
+    "options": ["True", "False"],
+    "answer": "False"
+  },
+  {
+    "q": "Marasmus is caused by the deficiency of carbohydrates and proteins.",
+    "options": ["True", "False"],
+    "answer": "True"
+  },
+  {
+    "q": "A balanced diet depends on the age, gender, and type of work a person does.",
+    "options": ["True", "False"],
+    "answer": "True"
+  },
+  {
+    "q": "We can reduce food miles by wasting food.",
+    "options": ["True", "False"],
+    "answer": "False"
+  },
+  {
+    "q": "Millets are also called nutri-cereals because of their health benefits.",
+    "options": ["True", "False"],
+    "answer": "True"
+  },
+  {
+    "q": "Roughage absorbs water and helps retain it in the body.",
+    "options": ["True", "False"],
+    "answer": "True"
+  },
+  {
+    "q": "Water helps in increasing the body temperature by sweating.",
+    "options": ["True", "False"],
+    "answer": "False"
+  },
+  {
+    "q": "Iodine is required for the formation of a red pigment called haemoglobin present in the blood.",
+    "options": ["True", "False"],
+    "answer": "False"
+  },
+  {
+    "q": "Vitamin D is a fat-soluble vitamin.",
+    "options": ["True", "False"],
+    "answer": "True"
+  },
+  {
+    "q": "The thick layer of fat in whales and dolphins that provides insulation is called fur.",
+    "options": ["True", "False"],
+    "answer": "False"
+  },
+  {
+    "q": "Fully submerged plants have weak roots and thin, ribbon-like leaves.",
+    "options": ["True", "False"],
+    "answer": "True"
+  },
+  {
+    "q": "A diet rich in fat can lead to a decrease in cholesterol levels.",
+    "options": ["True", "False"],
+    "answer": "False"
+  },
+  {
+    "q": "Cooking food at low temperatures destroys vitamins and minerals in it.",
+    "options": ["True", "False"],
+    "answer": "False"
+  },
+  {
+    "q": "A stronger magnet will always attract a weaker magnet, regardless of pole orientation.",
+    "options": ["True", "False"],
+    "answer": "False"
+  },
+  {
+    "q": "Similar poles of a magnet attract each other.",
+    "options": ["True", "False"],
+    "answer": "False"
+  }  
+];
+  
